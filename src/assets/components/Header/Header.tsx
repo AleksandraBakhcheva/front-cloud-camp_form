052608ee241c0e15d050779ca2b2f211ad7ab084
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-06-18 14:58:05 +0300

    change header data

--- src/assets/components/Header/Header.tsx
+++ src/assets/components/Header/Header.tsx
@@ -6,23 +6,26 @@ export default function Header() {
   return (
     <header className="header__container">
       <div className="header__container_box">
-        <div className="header__container_avatar">АИ</div>
+        <div className="header__container_avatar">АБ</div>
         <div className="header__container_info">
-          <h1>Иван Иванов</h1>
+          <h1>Александра Бахчева</h1>
           <div className="header__container_contacts">
-            <Link to="...">
+            <Link to="https://t.me/fromorningtonight" target="_blank">
               <div>
                 <img src={folder} alt="folder_icon_telegram" />
                 <p>Telegram</p>
               </div>
             </Link>
-            <Link to="...">
+            <Link to="https://github.com/AleksandraBakhcheva" target="_blank">
               <div>
                 <img src={folder} alt="folder_icon_github" />
                 <p>GitHub</p>
               </div>
             </Link>
-            <Link to="...">
+            <Link
+              to="https://hh.ru/resume/f07c2b80ff0b6607950039ed1f784b30557455"
+              target="_blank"
+            >
               <div>
                 <img src={folder} alt="folder_icon_resume" />
                 <p>Resume</p>
