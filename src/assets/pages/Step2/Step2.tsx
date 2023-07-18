555d5e429a6dd3216885670059219e42607c731f
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-07-18 21:24:34 +0300

    modify step2

--- src/assets/pages/Step2/Step2.tsx
+++ src/assets/pages/Step2/Step2.tsx
@@ -5,10 +5,12 @@ import { Button } from "../../components/Button/Button";
 import { Input } from "../../components/Input/Input";
 import { CheckboxRadio } from "../../components/CheckboxRadio/CheckboxRadio";
 import { Link, useNavigate } from "react-router-dom";
-import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import { observer } from "mobx-react";
+import store from "../../store";
+import { FieldArray, Form, Formik } from "formik";
 
-export default function Step2() {
+function Step2() {
   const advantagesRegExp = /^[A-zА-яЁё]+$/;
   const navigateNextPage = useNavigate();
 
@@ -17,156 +19,133 @@ export default function Step2() {
       <img src={stepper_2} alt="step_2" />
       <Formik
         initialValues={{
-          advantages1: "",
-          advantages2: "",
-          advantages3: "",
-          checked: [],
-          radio: "",
+          advantages: [
+            {
+              advantage: "",
+            },
+          ],
         }}
         validationSchema={Yup.object({
-          advantages1: Yup.string()
-            .max(50, "Максимальная длина 50 символов")
-            .matches(advantagesRegExp, "Можно использовать только буквы")
-            .required("Данное поле обязательно к заполнению"),
-          advantages2: Yup.string()
-            .max(50, "Максимальная длина 50 символов")
-            .matches(advantagesRegExp, "Можно использовать только буквы")
-            .required("Данное поле обязательно к заполнению"),
-          advantages3: Yup.string()
-            .max(50, "Максимальная длина 50 символов")
-            .matches(advantagesRegExp, "Можно использовать только буквы")
-            .required("Данное поле обязательно к заполнению"),
+          advantages: Yup.array().of(
+            Yup.object().shape({
+              advantage: Yup.string()
+                .max(30, "Максимальная длина 30 символов")
+                .matches(advantagesRegExp, "Можно использовать только буквы")
+                .required("Данное поле обязательно к заполнению"),
+            })
+          ),
         })}
         onSubmit={async (values, { setSubmitting }) => {
           await new Promise((r) => setTimeout(r, 500));
           setSubmitting(false);
-          localStorage.setItem("advantages1", values.advantages1);
-          localStorage.setItem("advantages2", values.advantages2);
-          localStorage.setItem("advantages3", values.advantages3);
-          //localStorage.setItem("checked", values.checked);
-          localStorage.setItem("radio", values.radio);
           navigateNextPage("/step3");
         }}
       >
-        <Form>
-          <div className="user-input">
-            <label>Ваши сильные стороны</label>
-            <div>
-              <div>
-                <img
-                  src={button_delete}
-                  alt="button-delete"
-                  id="button-remove-1"
+        {({ values }) => (
+          <Form>
+            <div className="user-input">
+              <label>Ваши сильные стороны</label>
+              <FieldArray name="advantages">
+                {({ remove, push }) => (
+                  <>
+                    {values.advantages.length > 0 &&
+                      values.advantages.map((item, index) => (
+                        <div key={index}>
+                          <div>
+                            <img
+                              src={button_delete}
+                              alt="button-delete"
+                              id={`button-remove-${index + 1}`}
+                              onClick={() => remove(index)}
+                            />
+                          </div>
+                          <Input
+                            key={index}
+                            id={`field-advantages-${index + 1}`}
+                            name={`advantages.${index}.advantage`}
+                            placeholder="Введите навык"
+                            type="text"
+                          />
+                        </div>
+                      ))}
+                    <Button
+                      type="button"
+                      id="button-add"
+                      onClick={() => push({ advantage: "" })}
+                    >
+                      +
+                    </Button>
+                  </>
+                )}
+              </FieldArray>
+            </div>
+            <div className="step2__container_checkboxes">
+              <div id="checkbox-group">Checkbox group</div>
+              <div role="group" aria-labelledby="checkbox-group">
+                <CheckboxRadio
+                  id="field-checkbox-group-option-1"
+                  name="checked"
+                  value="1"
+                  label="1"
+                  inputtype="checkbox"
+                />
+                <CheckboxRadio
+                  id="field-checkbox-group-option-2"
+                  name="checked"
+                  value="2"
+                  label="2"
+                  inputtype="checkbox"
+                />
+                <CheckboxRadio
+                  id="field-checkbox-group-option-3"
+                  name="checked"
+                  value="3"
+                  label="3"
+                  inputtype="checkbox"
                 />
               </div>
-              <Input
-                label=""
-                id="field-advantages-1"
-                name="advantages1"
-                type="text"
-                placeholder="Введите навык"
-              />
             </div>
-            <div>
-              <div>
-                <img
-                  src={button_delete}
-                  alt="button-delete"
-                  id="button-remove-2"
+            <div className="step2__container_radios">
+              <div id="radio-group">Radio group</div>
+              <div role="group" aria-labelledby="radio-group">
+                <CheckboxRadio
+                  id="field-radio-group-option-1"
+                  name="radio"
+                  value="1"
+                  label="1"
+                  inputtype="radio"
+                />
+                <CheckboxRadio
+                  id="field-radio-group-option-2"
+                  name="radio"
+                  value="2"
+                  label="2"
+                  inputtype="radio"
+                />
+                <CheckboxRadio
+                  id="field-radio-group-option-3"
+                  name="radio"
+                  value="3"
+                  label="3"
+                  inputtype="radio"
                 />
               </div>
-              <Input
-                label=""
-                id="field-advantages-2"
-                name="advantages2"
-                type="text"
-                placeholder="Введите навык"
-              />
             </div>
-            <div>
-              <div>
-                <img
-                  src={button_delete}
-                  alt="button-delete"
-                  id="button-remove-3"
-                />
-              </div>
-              <Input
-                label=""
-                id="field-advantages-3"
-                name="advantages3"
-                type="text"
-                placeholder="Введите навык"
-              />
-            </div>
-            <Button type="button" id="button-add">
-              +
-            </Button>
-          </div>
-          <div className="step2__container_checkboxes">
-            <div id="checkbox-group">Checkbox group</div>
-            <div role="group" aria-labelledby="checkbox-group">
-              <CheckboxRadio
-                id="field-checkbox-group-option-1"
-                name="checked"
-                value="1"
-                label="1"
-                inputtype="checkbox"
-              />
-              <CheckboxRadio
-                id="field-checkbox-group-option-2"
-                name="checked"
-                value="2"
-                label="2"
-                inputtype="checkbox"
-              />
-              <CheckboxRadio
-                id="field-checkbox-group-option-3"
-                name="checked"
-                value="3"
-                label="3"
-                inputtype="checkbox"
-              />
-            </div>
-          </div>
-          <div className="step2__container_radios">
-            <div id="radio-group">Radio group</div>
-            <div role="group" aria-labelledby="radio-group">
-              <CheckboxRadio
-                id="field-radio-group-option-1"
-                name="radio"
-                value="1"
-                label="1"
-                inputtype="radio"
-              />
-              <CheckboxRadio
-                id="field-radio-group-option-2"
-                name="radio"
-                value="2"
-                label="2"
-                inputtype="radio"
-              />
-              <CheckboxRadio
-                id="field-radio-group-option-3"
-                name="radio"
-                value="3"
-                label="3"
-                inputtype="radio"
-              />
-            </div>
-          </div>
-          <div className="step2__container_buttons">
-            <Link to="/step1">
-              <Button type="submit" id="button-back">
-                Назад
+            <div className="step2__container_buttons">
+              <Link to="/step1">
+                <Button type="submit" id="button-back">
+                  Назад
+                </Button>
+              </Link>
+              <Button type="submit" id="button-next">
+                Далее
               </Button>
-            </Link>
-            <Button type="submit" id="button-next">
-              Далее
-            </Button>
-          </div>
-        </Form>
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
 }
+
+export default observer(Step2);
