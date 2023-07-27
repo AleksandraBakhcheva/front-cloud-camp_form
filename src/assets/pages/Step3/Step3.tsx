0d0b711361271527a35a0c6ee57c8701470e1693
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-07-27 21:17:46 +0300

    restructure step 3 and add counter + styles for textarea

--- src/assets/pages/Step3/Step3.tsx
+++ src/assets/pages/Step3/Step3.tsx
@@ -1,53 +1,25 @@
 import "./Step3.scss";
-import stepper_3 from "../../images/stepper_3.svg";
+import { ProgressBar } from "../../components/ProgressBar/ProgressBar";
 import { Button } from "../../components/Button/Button";
 import { Link } from "react-router-dom";
 import { ModalSuccess } from "../../components/Modal/ModalSuccess";
 import { ModalFail } from "../../components/Modal/ModalFail";
-import { PostData } from "../../utils/types";
-import { newUser } from "../../utils/user";
 import { TextArea } from "../../components/TextArea/TextArea";
 import { Formik, Form } from "formik";
 import { useState } from "react";
 import * as Yup from "yup";
+import { observer } from "mobx-react";
+import store from "../../store";
+import { User } from "../../utils/user";
 
-export default function Step3() {
-  const endPoint = "https://api.sbercloud.ru/content/v1/bootcamp/frontend";
+function Step3() {
   const [modalSuccess, setModalSuccess] = useState(false);
   const [modalFail, setModalFail] = useState(false);
-
-  async function sendDataToServer(data: PostData) {
-    try {
-      const newUser = await fetch(endPoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-        },
-        body: JSON.stringify(data),
-      });
-      const responseReceived = await newUser.json();
-      if (responseReceived.status) {
-        setModalSuccess(true);
-        const windowBackground =
-          document.querySelector<any>(".step3__container");
-        if (windowBackground) {
-          windowBackground.style.opacity = 0.3;
-        }
-      }
-    } catch (error) {
-      console.log("error", error);
-      setModalFail(true);
-      const windowBackground = document.querySelector<any>(".step3__container");
-      if (windowBackground) {
-        windowBackground.style.opacity = 0.3;
-      }
-    }
-  }
 
   return (
     <div>
       <div className="step3__container">
-        <img src={stepper_3} alt="step_3" />
+        <ProgressBar step={3} />
         <Formik
           initialValues={{
             about: "",
@@ -59,31 +31,38 @@ export default function Step3() {
           })}
           onSubmit={async (values, { setSubmitting }) => {
             await new Promise((r) => setTimeout(r, 500));
-            localStorage.setItem("about", values.about);
             setSubmitting(false);
-            sendDataToServer(newUser);
+            /*  store.formStore.about = values.about;
+            console.log(values.about);
+            store.formStore.sendDataToServer(User); */
           }}
         >
-          <Form>
-            <div className="user-input">
-              <TextArea
-                label="О себе"
-                name="about"
-                id="field-about"
-                placeholder="Расскажите о себе"
-              />
-            </div>
-            <div className="step3__container_buttons">
-              <Link to="/step2">
-                <Button type="submit" id="button-back">
-                  Назад
+          {({ values }) => (
+            <Form>
+              <div className="user-input">
+                <TextArea
+                  label="О себе"
+                  name="about"
+                  id="field-about"
+                  placeholder="Расскажите о себе"
+                />
+                <div className="user-input_counter">
+                  {values.about.split(" ").join("").length} / 200
+                </div>
+              </div>
+
+              <div className="step3__container_buttons">
+                <Link to="/step2">
+                  <Button type="submit" id="button-back">
+                    Назад
+                  </Button>
+                </Link>
+                <Button id="button-send" type="submit">
+                  Отправить
                 </Button>
-              </Link>
-              <Button id="button-send" type="submit">
-                Отправить
-              </Button>
-            </div>
-          </Form>
+              </div>
+            </Form>
+          )}
         </Formik>
       </div>
       <div>{modalSuccess ? <ModalSuccess /> : ""}</div>
@@ -91,3 +70,5 @@ export default function Step3() {
     </div>
   );
 }
+
+export default observer(Step3);
